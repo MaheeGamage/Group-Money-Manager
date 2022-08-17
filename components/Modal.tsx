a4d756ad4a0c4c22765aa8350e58610777da19b9
MaheeGamage <mahee.gamage@gmail.com>
2022-08-17 23:03:05 +0530

    Modal component updated to HoC

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
-const Modal = () => {
+type ModelProps = {
+    children: React.ReactNode,
+  };
+
+const Modal = ({children}: ModelProps) => {
     const [showModal, setShowModal] = useState(true);
 
     const renderModal = () => (
         // TODO - use gloabl css class to put these class based css styles for modal
         <div className="modal opacity-100 visible pointer-events-auto">
-            <div className="modal-box">
-                <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-                <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                <div className="modal-action">
-                    <label className="btn" onClick={() => setShowModal(false)}>Yay!</label>
-                </div>
+            <div className="modal-box relative">
+                <div className="btn btn-sm btn-circle absolute right-2 top-2">✕</div>
+                {children}
             </div>
         </div>
     )
