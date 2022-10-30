4629747c73eea0d39e633a3f3902ea840d8132da
MaheeGamage <mahee.gamage@gmail.com>
2022-10-30 18:56:45 +0530

    Remove unnecessary html items

--- layouts/navWithSidebarLayout.tsx
+++ layouts/navWithSidebarLayout.tsx
@@ -25,12 +25,11 @@ const NavWithSidebarLayout = ({ children }: NavLayoutProps) => {
 									</div>
 								</a>
 							</div>
-							<div className="flex-1"></div>
-							<div className="flex-none">
+							{/* <div className="flex-none">
 								<button className="btn btn-square btn-ghost">
 									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
 								</button>
-							</div>
+							</div> */}
 						</div>
 					</div>
 					{/* Content */}
@@ -49,7 +48,7 @@ const NavWithSidebarLayout = ({ children }: NavLayoutProps) => {
 						</div>
 
 						<ul className="menu p-4 overflow-y-auto bg-base-100 text-base-content">
-							<li><a>Main</a></li>
+							<li><a>Budget Calculator</a></li>
 						</ul>
 					</aside>
 				</div>
